d04d7de5c0b7550242391ad943f4a41f46973bd0
Segelzwerg <marcel.haas@hhu.de>
2020-12-03 13:26:38 +0100

    ADD: day 1

--- Year2020/Day1/expenses.py
+++ Year2020/Day1/expenses.py
@@ -1,6 +1,26 @@
-def expenses(input: [int]):
-    for x in input:
-        for y in input:
+import numpy as np
+
+
+def expenses(input_array: [int]):
+    for x in input_array:
+        for y in input_array:
             if x + y == 2020:
                 print(f'x={x}, y={y}')
                 return x * y
+
+def three_sum(input_array: [int]) -> int:
+    for x in input_array:
+        for y in input_array:
+            for z in input_array:
+                if x + y + z == 2020:
+                    print(f'x={x}, y={y}, z={z}')
+                    return x * y * z
+
+def main():
+    input_array = np.genfromtxt('input.txt', dtype=int)
+    print(expenses(input_array))
+    print(three_sum(input_array))
+
+
+if __name__ == '__main__':
+    main()
--- Year2020/Day1/test_expenses.py
+++ Year2020/Day1/test_expenses.py
@@ -1,10 +1,14 @@
 from unittest import TestCase
 
-from Year2020.Day1.expenses import expenses
+from Year2020.Day1.expenses import expenses, three_sum
 
 
 class ExpensesTestCase(TestCase):
-    def test_with_example(self):
-        input = (1721, 979, 366, 299, 675, 1456)
-        self.assertEqual(514579, expenses(input))
+    def setUp(self) -> None:
+        self.input_array = (1721, 979, 366, 299, 675, 1456)
 
+    def test_with_example(self):
+        self.assertEqual(514579, expenses(self.input_array))
+
+    def test_three_sum(self):
+        self.assertEqual(241861950, three_sum(self.input_array))
